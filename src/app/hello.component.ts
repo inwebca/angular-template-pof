63aff8f42b8ca1f24ce06d944949b69d1fe515bb
inwebca <blouinolivier@gmail.com>
2021-02-26 14:02:28 -0500

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit {
+  componentList: ComponentType[];
+
   @Input() name: string;
+
+  ngOnInit() {
+    this.componentList = [
+      new ComponentType(1, "dropdown"),
+      new ComponentType(2, "checkbox")
+    ];
+
+    const componentList$ = of(this.componentList);
+
+    componentList$.sub;
+  }
+}
+
+export class ComponentType {
+  constructor(id, kind) {
+    this.id = id;
+    this.kind = kind;
+  }
+  id: number;
+  kind: string;
 }
