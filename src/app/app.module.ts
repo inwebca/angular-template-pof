964db3a15609ddfd0def111fdc3d22b71d1143f5
inwebca <blouinolivier@gmail.com>
2021-03-03 10:08:02 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { MatSelectModule } from "@angular/material/select";
 import { MatCheckboxModule } from "@angular/material/checkbox";
@@ -10,9 +11,11 @@ import { AppComponent } from "./app.component";
 import { SurveyComponent } from "./survey-component/survey.component";
 import { DropdownComponent } from "./dropdown-component/dropdown.component";
 import { CheckboxComponent } from "./checkbox-component/checkbox.component";
+import { SurveyDialogComponent } from "./survey-dialog-component/survey-dialog.component";
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     MatSelectModule,
@@ -24,7 +27,8 @@ import { CheckboxComponent } from "./checkbox-component/checkbox.component";
     AppComponent,
     SurveyComponent,
     DropdownComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    SurveyDialogComponent
   ],
   bootstrap: [AppComponent]
 })
