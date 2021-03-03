15e60525bca26275101eebe04e2863a09a10ff3c
inwebca <blouinolivier@gmail.com>
2021-03-03 08:42:14 -0500

    1 file modified

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -6,69 +6,22 @@ import { Observable, of } from "rxjs";
 })
 export class SurveyService {
   public driverSurveys(): Observable<IDriverSurvey[]> {
-    let driverSurveys = Array<IDriverSurvey> = [
+    let driverSurveysArray: Array<IDriverSurvey> = [
       { 
         startDate: "2021-01-01", 
         endDate: "2021-02-01",
         savedDate: "2021-01-01", 
         driver: {id: 1, name: "Test"},
         question: [
-          {id : 1, displayName: "test", choosenMin: 1, choosenMax: 2}: IMinMaxQuestion
+          
+
         ]
        }
     ]
 
-    return of(driverSurveys);
+    return of(driverSurveysArray);
 
   }
-
-  public getSurveys(): Observable<ICriteriaKit[]> {
-    let criteriaKits: Array<ICriteriaKit> = [
-      {
-        id: 1,
-        name: "dropdown",
-        kind: Type.DROPDOWN,
-        question: "question1",
-        details: [
-          {
-            id: 1,
-            label: "choice1",
-            value: "choice1"
-          },
-          {
-            id: 2,
-            label: "choice2",
-            value: "choice2"
-          }
-        ]
-      },
-      {
-        id: 2,
-        name: "checkbox",
-        kind: Type.CHECKBOX,
-        question: "question2",
-        details: [
-          {
-            id: 1,
-            label: "choice1",
-            value: true
-          },
-          {
-            id: 2,
-            label: "choice2",
-            value: true
-          },
-          {
-            id: 3,
-            label: "choice2",
-            value: false
-          }
-        ]
-      }
-    ];
-    return of(criteriaKits);
-  }
-}
 
 export interface IDriverSurvey {
   startDate: string;
