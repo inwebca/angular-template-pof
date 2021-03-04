d7e6ba5e03cbb62f61b4d88cc266b8cefb60bd15
inwebca <blouinolivier@gmail.com>
2021-03-04 08:49:55 -0500

    2 files modified

--- src/app/survey-dialog-component/survey-dialog.component.ts
+++ src/app/survey-dialog-component/survey-dialog.component.ts
@@ -54,7 +54,6 @@ export class SurveyDialogComponent implements OnInit {
       }
     });
 
-    console.log(this.formGroup);
     console.log(this.form.controls);
 
     //const test = new FormGroup(group);
